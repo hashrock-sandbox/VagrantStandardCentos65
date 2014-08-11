e28545905e3bbfbf52fdb6a0ded4fbc553a1fad8
181dev <181dev@users.noreply.github.com>
2014-08-11 19:11:26 +0900

    install ruby command(comment-outed)

--- vagrant/user-config.sh
+++ vagrant/user-config.sh
@@ -4,3 +4,12 @@ source ~/.bash_profile
 nodebrew install-binary stable
 nodebrew use stable
 npm install -g grunt-cli express-generator bower node-dev
+
+#Install Ruby
+#git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+#echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+#echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+#source ~/.bash_profile
+#git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+#rbenv install 2.1.0
+#rbenv global 2.1.0
