3e9e3c473df287db7d15c9cc0b78403fc24d76a3
181dev <181dev@users.noreply.github.com>
2014-08-11 19:37:02 +0900

    add mongodb

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -1,3 +1,17 @@
 sudo yum install -y git
 sudo yum install -y gcc-c++ glibc-headers openssl-devel readline libyaml-devel readline-devel zlib zlib-devel
+
+touch /etc/yum.repos.d/mongodb.repo
+echo "[mongodb]" >> /etc/yum.repos.d/mongodb.repo
+echo "name=MongoDB Repository" >> /etc/yum.repos.d/mongodb.repo
+echo "baseurl=http://downloads-distro.mongodb.org/repo/redhat/os/x86_64/" >> /etc/yum.repos.d/mongodb.repo
+echo "gpgcheck=0" >> /etc/yum.repos.d/mongodb.repo
+echo "enabled=1" >> /etc/yum.repos.d/mongodb.repo
+
+sudo yum install -y mongodb-org
+sudo mkdir -p /data/db
+sudo service mongod start
+sudo chkconfig mongod on
+
 su -c "source /home/vagrant/standard/vagrant/user-config.sh" vagrant
+
